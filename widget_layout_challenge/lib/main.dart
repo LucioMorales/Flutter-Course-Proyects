93ef7d60d5900f96ffb563229598cfeca9ab5b2c
Lucio Morales <lucio.moralesdemaria@gmail.com>
2021-07-08 11:36:47 -0300

    widget_layout_challenge uploaded

---
+++ widget_layout_challenge/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal[700],
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Container(
+              color: Colors.red,
+              width: 100,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.yellow,
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.green,
+                )
+              ],
+            ),
+            Container(
+              color: Colors.blue,
+              width: 100,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
