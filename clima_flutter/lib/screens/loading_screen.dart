eb33b6a51d8ed32fa5c536bd2a3cc08bdb2a5ecf
Lucio Morales <lucio.moralesdemaria@gmail.com>
2021-08-17 18:17:23 -0300

    added an api call to request weather

--- clima_flutter/lib/screens/loading_screen.dart
+++ clima_flutter/lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:clima_flutter/services/location.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,26 +8,37 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState(){
+    super.initState();
+    getLocation();
+  }
 
-  void getLocation() async{
-    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-    print (position);
-    LocationPermission permission = await Geolocator.requestPermission();
-    print(permission);
+  void getLocation() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+    print(location.latitude);
+    print(location.longitude);
+  }
+
+  void getData() async {
+    http.Response response = await http.get(
+      Uri.parse(
+          'http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=6ca68aaffd9aca915c7f31a2a023cef2'),
+    );
+    
+    if (response.statusCode == 200){
+      String data = response.body;
+      print(data);
+    } else{
+      print(response.statusCode);
+    }
 
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            getLocation();
-          },
-          child: Text('Get Location'),
-        ),
-      ),
-    );
+    getData();
+    return Scaffold();
   }
 }
